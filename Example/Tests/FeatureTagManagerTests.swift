f93a95f73ea93c52f032ce8a3b06ecc3cec83541
Eliran Ben-Ezra <eliran@gc.io>
2018-11-01 22:50:40 -0400

    adding more unit tests

--- Example/Tests/FeatureTagManagerTests.swift
+++ Example/Tests/FeatureTagManagerTests.swift
@@ -12,6 +12,9 @@ class FeatureTagManagerTests: XCTestCase {
     let f1 = makeFeature()
     let f2 = makeFeature(key: "123")
     let f3 = makeFeature(enabled: true)
+    let ignoreBool = true
+    let ignoreString = "123"
+    let ignoreInt = 123
   }
 
   class OtherTestFeatures {
@@ -39,6 +42,11 @@ class FeatureTagManagerTests: XCTestCase {
 
   func test_that_instance_always_returns_the_same_instance() {
     expect(FeatureTags.Manager.instance) === FeatureTags.Manager.instance
+  }
+
+  func test_that_non_feature_tag_instance_vars_are_ignored() {
+    manager.register(testFeatures)
+    expect(self.manager.allFeatures.map { $0.name }).toNot(contain(["ignoreBool", "ignoreString", "ignoreInt"]))
   }
 
   func test_that_registered_features_have_the_correct_name_space() {
@@ -71,6 +79,35 @@ class FeatureTagManagerTests: XCTestCase {
     expect(self.testFeatures.f1.isOn).to(beTrue())
     mockGetter.value = [:]
     expect(self.testFeatures.f1.isOn).to(beFalse())
+  }
+
+  func test_that_features_return_the_default_value() {
+    manager.register(testFeatures)
+    expect(self.testFeatures.f1.isOn).to(beFalse())
+    expect(self.testFeatures.f3.isOn).to(beTrue())
+  }
+
+  func test_that_features_return_the_values_source() {
+    manager.install(name: "local", priority: 1, getter: mockGetter)
+    manager.register(testFeatures)
+    expect(self.testFeatures.f1.source).to(equal("default"))
+    mockGetter.value[testFeatures.f1] = true
+    expect(self.testFeatures.f1.source).to(equal("local"))
+  }
+
+  func test_that_feature_tags_can_move_to_a_new_manager() {
+    manager.register(testFeatures)
+    manager.install(name: "local", priority: 1, getter: mockGetter)
+    mockGetter.value[testFeatures.f1] = true
+    let otherManager = FeatureTags.Manager()
+    otherManager.register(testFeatures)
+    expect(self.testFeatures.f1.isOn).to(beFalse())
+  }
+
+  func test_that_feature_tags_can_be_registered_multiple_times_to_the_same_manager() {
+    manager.register(testFeatures)
+    manager.register(testFeatures)
+    expect(self.manager.allFeatures.count).to(equal(3))
   }
 
   func test_that_resolvers_are_evaluated_in_asending_priority_order() {
---
+++ Example/Tests/LocalResolverTests.swift
@@ -0,0 +1,37 @@
+//
+//  LocalResolverTests.swift
+//  TPFeatureTag_Example
+//
+//  Created by Eliran Ben-Ezra on 11/1/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import Nimble
+@testable import TPFeatureTag
+
+class LocalResolverTests: XCTestCase {
+  var resolver: (FeatureTagGetter & FeatureTagSetter)!
+  override func setUp() {
+    super.setUp()
+    resolver = FeatureTags.Resolvers.Local(nameSpace: "test")
+  }
+
+  func test_that_feature_tag_value_can_be_set() {
+    let tag = FeatureTag(key: "1234", enabled: false)
+    resolver.clear(feature: tag)
+    expect(self.resolver.isOn(feature: tag)).to(beNil())
+    resolver.set(feature: tag, isOn: true)
+    expect(self.resolver.isOn(feature: tag)).to(beTrue())
+    resolver.set(feature: tag, isOn: false)
+    expect(self.resolver.isOn(feature: tag)).to(beFalse())
+  }
+
+  func test_that_feature_tag_can_be_cleared() {
+    let tag = FeatureTag(key: "5431", enabled: false)
+    resolver.set(feature: tag, isOn: true)
+    expect(self.resolver.isOn(feature: tag)).to(beTrue())
+    resolver.clear(feature: tag)
+    expect(self.resolver.isOn(feature: tag)).to(beNil())
+  }
+}
